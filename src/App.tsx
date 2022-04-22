7f98dbf499c7a11e760e06b818423d75c2d6c516
Rachael Morris <rachael.morris@sky.uk>
2022-04-22 20:28:17 +0100

    update styling

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,9 @@ export const App: FunctionComponent = () => {
           <img className="rounded-2xl" src={`${process.env.PUBLIC_URL}/images/image-equilibrium.jpg`} alt="QR code" />
         </div>
 
-        <h1 className="text-white text-xl font-bold cursor-pointer hover:text-primary-cyan transition-all duration-300 ease-in-out">Equilibrium #3429</h1>
+        <h1 className="text-white text-xl font-bold cursor-pointer hover:text-primary-cyan transition-all duration-300 ease-in-out leading-none">Equilibrium #3429</h1>
         <p>Our Equilibrium collection promotes balance and calm.</p>
-        <div className="flex justify-between text-sm">
+        <div className="flex justify-between text-sm font-semibold">
           <div className="text-primary-cyan flex place-items-center gap-2">
             <img src={`${process.env.PUBLIC_URL}/images/icon-ethereum.svg`} alt="Ethereum" />
             0.041 ETH
@@ -24,8 +24,8 @@ export const App: FunctionComponent = () => {
           </div>
         </div>
         <hr className="border-blue-line" />
-        <div className="flex place-items-center gap-5">
-          <img className="rounded-full border max-h-10" src={`${process.env.PUBLIC_URL}/images/image-avatar.png`} alt="Avatar" />
+        <div className="flex place-items-center gap-4 text-sm">
+          <img className="rounded-full border max-h-8" src={`${process.env.PUBLIC_URL}/images/image-avatar.png`} alt="Avatar" />
           <div>
             Creation of <p className="text-white inline cursor-pointer hover:text-primary-cyan transition-all duration-300 ease-in-out">Jules Wyvern</p>
           </div>
